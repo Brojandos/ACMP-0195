cbb9116c080bcca57de1a3ae3879561d6758a11e
Brojandos <Brojandos@gmail.com>
2017-02-19 11:52:16 +0600

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+/**
+ * @author: Brojandos.
+ * @creation_date: 19.02.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println(in.nextByte() * in.nextByte() * in.nextByte() * 2);
+    }
+}
